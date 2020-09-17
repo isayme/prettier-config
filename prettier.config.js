453b9f13fb63f25e314988984e1f8290f4ba2c6f
isayme <isaymeorg@gmail.com>
2020-09-17 21:09:48 +0800

    update

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,14 @@
 // prettier options: https://prettier.io/docs/en/options.html
 module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+
+  // default settings
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
-  semi: false,
-  singleQuote: true,
   quoteProps: 'as-needed',
-  trailingComma: 'all',
   bracketSpacing: true,
   arrowParens: 'always',
   endOfLine: 'lf',
