ae7d764d53b9766612adae54ea8322344ab1d255
isayme <isaymeorg@gmail.com>
2020-09-17 20:45:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# @isayme/prettier-config
+[Prettier](https://prettier.io) 支持[共享配置](https://prettier.io/docs/en/configuration.html#sharing-configurations).
+
+使用方只需要升级版本即可更新至最新的配置模式, 避免配置变更时, 使用方需要复制新的配置项.
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// prettier options: https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
