0abe2390bb2e7a3ee91375871cde30b7688663c7
isayme <isaymeorg@gmail.com>
2020-10-09 20:17:03 +0800

    add option

--- prettier.config.js
+++ prettier.config.js
@@ -2,12 +2,16 @@
 module.exports = {
   semi: false,
   singleQuote: true,
+  jsxSingleQuote: true,
   trailingComma: 'all',
 
   // default settings
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
+  jsxBracketSameLine: false,
+  requirePragma: false,
+  insertPragma: false,
   quoteProps: 'as-needed',
   bracketSpacing: true,
   arrowParens: 'always',
